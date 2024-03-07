c5d360ab3ebee43cfdd1225d80478d43ed54894c
AGBOIHIN Hippolyte <hippolyte.agboihin@coexya.eu>
2024-03-07 16:47:49 +0100

    Peupler la base de donnée de test avec des données samples

---
+++ lunt-backoffice/sql/samples-data/auteur.sql
@@ -0,0 +1,3 @@
+insert into unt.auteur (id, nom, prenom, email, cree_le, edite_le)
+values  (1, 'STEEVE', 'Anne', 'john@doe.eu', '2024-02-02 10:16:00', null),
+        (2, 'MICHAEL', 'Bob', 'bob.michael@gmail.fr', '0000-00-00 00:00:00', null);
---
+++ lunt-backoffice/sql/samples-data/notice_notice.sql
@@ -0,0 +1,4 @@
+insert into unt.notice_notice (notice_source, notice_target)
+values  (1, 1),
+        (18, 16),
+        (31, 1);
