e7e48a95adab5b4272c1388ba62fdd7eb699efbd
Louis Ferret <louis.ferret@coexya.eu>
2025-06-13 11:58:43 +0200

    Feat[UIN-170] : Ajout de la Table deweyPerso en cas de code dewey personnaliser dans les XML.

---
+++ lunt-backoffice/src/Entity/DeweyPerso.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Entity;
+
+use App\Repository\DeweyPersoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DeweyPersoRepository::class)]
+class DeweyPerso
+{
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private ?int $id = null;
+
+  #[ORM\Column(length: 255, unique: true)]
+  private ?string $code = null;
+
+  #[ORM\Column(length: 255)]
+  private ?string $nom = null;
+
+  public function getId(): ?int { return $this->id; }
+  public function getCode(): ?string { return $this->code; }
+  public function setCode(string $code): static { $this->code = $code; return $this; }
+  public function getNom(): ?string { return $this->nom; }
+  public function setNom(string $nom): static { $this->nom = $nom; return $this; }
+
+  public function __toString(): string { return $this->nom ?? ''; }
+}
---
+++ lunt-backoffice/src/Repository/DeweyPersoRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Repository;
+
+use App\Entity\DeweyPerso;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+class DeweyPersoRepository extends ServiceEntityRepository
+{
+  public function __construct(ManagerRegistry $registry)
+  {
+    parent::__construct($registry, DeweyPerso::class);
+  }
+}
