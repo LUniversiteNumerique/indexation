ec548861b14ec71802b79fb6f890cb4953b1d6f7
Louis Ferret <louis.ferret@coexya.eu>
2025-06-17 16:38:58 +0200

    Feat[UIN-170] : modification d'import sur les auteurs, spécialités, deweys...

--- lunt-backoffice/src/Command/ImportNoticeXmlCommand.php
+++ lunt-backoffice/src/Command/ImportNoticeXmlCommand.php
@@ -5,6 +5,7 @@ namespace App\Command;
 use App\Entity\Dto\{Classification, Contribute, Field, Motcle, Relation, SuplomDto, Taxon};
 use App\Entity\{Auteur,
   Dewey,
+  DeweyPerso,
   Discipline,
   Etablissement,
   Keyword,
@@ -36,7 +37,7 @@ class ImportNoticeXmlCommand extends Command
 {
   private FileService $fs;
   private array $droiRep, $niveRep, $tdocRep, $tpedRep;
-  private EntityRepository $userRep, $deweRep, $auteRep, $kwrdRep, $discRep, $etabRep;
+  private EntityRepository $userRep, $deweRep, $auteRep, $kwrdRep, $discRep, $etabRep, $dewePersoRep;
   private Filesystem $filesystem;
 
   public function __construct(private readonly EntityManagerInterface $em, private readonly SerializerInterface $serializer)
@@ -45,6 +46,7 @@ class ImportNoticeXmlCommand extends Command
     parent::__construct();
     $this->userRep = $this->em->getRepository(User::class);
     $this->deweRep = $this->em->getRepository(Dewey::class);
+    $this->dewePersoRep = $this->em->getRepository(DeweyPerso::class);
     $this->auteRep = $this->em->getRepository(Auteur::class);
     $this->kwrdRep = $this->em->getRepository(Keyword::class);
     $this->discRep = $this->em->getRepository(Discipline::class);
@@ -85,6 +87,18 @@ class ImportNoticeXmlCommand extends Command
     $finder->files()->in($path)->name($names)->depth($deep);
     if ($since) $finder->date('>= ' . $since->format('Y-m-d H:i:s'));
     $data = $finder;
+
+    //Suppression de l'enssemble des notices en base actuel
+    $notices = $this->em->getRepository(Notice::class)->createQueryBuilder('n')
+      ->where('n.id >= :id')
+      ->setParameter('id', 4)
+      ->getQuery()
+      ->getResult();
+
+    foreach ($notices as $notice) {
+      $this->em->remove($notice);
+    }
+    $this->em->flush();
 
     $io->progressStart(count($data));
     $nr = $this->em->getRepository(Notice::class);
@@ -134,8 +148,10 @@ class ImportNoticeXmlCommand extends Command
         }
       }
 
-      $notice->setCreateur($this->userRep->findOneBy(['name' => $roleNotice["creator"]]))
-        ->setValidateur($this->userRep->findOneBy(['name' => $roleNotice["validator"]]));
+      $creatorName = $roleNotice["creator"][0] ?? null;
+      $notice->setCreateur($this->userRep->findOneBy(['name' => $creatorName]));
+      $validatorName = $roleNotice["validator"][0] ?? null;
+      $notice->setValidateur($this->userRep->findOneBy(['name' => $validatorName]));
 
       // Ajouter un utilisateur par défaut quand une notice en a pas
       if ($notice->getCreateur() == null) {
@@ -145,7 +161,7 @@ class ImportNoticeXmlCommand extends Command
         ];
         $existingUser = $this->userRep->findOneBy(['name' => 'créateur inconnu']);
         if ($existingUser === null) {
-          $entity = new User('créateur inconnu', 'créateurinconnu@créateurinconnu.com');
+          $entity = new User('créateur inconnu', 'noreply@luniversitenumerique.fr');
           $this->em->persist($entity);
           $this->em->flush();
           $notice->setCreateur($entity);
@@ -175,7 +191,15 @@ class ImportNoticeXmlCommand extends Command
           $lName = implode(' ', array_slice($name, 0, -1));
           $fName = $name[$cName - 1];
         }
-        if ($cName = $this->auteRep->findOneBy(['nom' => $lName, 'prenom' => $fName])) $notice->addAuteur($cName);
+        $cName = $this->auteRep->findOneBy(['nom' => $lName, 'prenom' => $fName]);
+        if ($cName) {
+          $notice->addAuteur($cName);
+        } else {
+          $entity = new Auteur($lName, $fName);
+          $this->em->persist($entity);
+          $this->em->flush();
+          $notice->addAuteur($entity);
+        }
       }
       array_map(fn(Etablissement $etab) => $notice->addPorteur($etab), $this->etabRep->findBy(['nom' => $roleNotice["publisher"]]));
       array_map(fn(Keyword $kywd) => $notice->addTag($kywd), $this->kwrdRep->findBy(['nom' => $motcles]));
@@ -186,15 +210,18 @@ class ImportNoticeXmlCommand extends Command
         ->setDureExec($item->technical?->duration->duration ?? null)
         ->setRessLang($item->general->languages)
         ->setUserLang($item->educational->languages)
-        ->setDureAppr($item->educational->typicalLearningTime->duration ?? null)
+        ->setDureAppr(normalizeDuration($item->educational->typicalLearningTime->duration ?? null))
         ->setProprIntel(strtolower($item->rights?->copyrightAndOtherRestrictions?->value ?? '') !== "no")
         ->setRessPayant(strtolower($item->rights?->cost?->value ?? '') !== "no")
         ->setPropUser(array_map(fn(Motcle $s) => $s->string?->value, $item->educational?->description))
         ->setDroit($this->droiRep[strtolower(preg_replace('/\s+/', '', $item->rights?->description[0]?->value))]);
 
       if (str_contains(trim($item->technical?->location), 'document/')) {
+        $notice->setRessUrl(trim($item->technical?->location));
         //$notice->setRessZip(trim($item->technical?->location));
-        $output->writeln("Lien trouvé pour format ZIP : " . $notice->getRessUrl());
+        //$output->writeln("Lien trouvé pour format ZIP : " . $notice->getRessUrl() . "\n");
+        $output->writeln("Lien trouvé pour format ZIP : " . trim($item->technical?->location) . "\n");
+        $output->writeln("uuid : ". $notice->getUuid() . "\n");
       } else {
         $notice->setRessUrl(trim($item->technical?->location));
       }
@@ -207,28 +234,102 @@ class ImportNoticeXmlCommand extends Command
       foreach ($item->classifications as $class) {
         if (isset($class->taxonPath)) {
           $key = array_reduce($class->taxonPath->source, fn(string $a, Field $s) => "$a $s->value", "");
-          $value = array_map(fn(Taxon $taxon) => $taxon->entry[0]?->value, $class->taxonPath->taxons);
 
-          if (str_contains($key, 'lassification')) array_map(function (Discipline $spec) use ($notice) {
-            $disc = $spec->getParent();
-            if ($disc?->getParent()) $notice->addSpecialite($spec);
-          }, $this->discRep->findBy(['nom' => $value]));
-          if (str_contains($key, 'CDD 22')) array_map(function (Dewey $spec) use ($notice) {
-            $disc = $spec->getParent();
-            if ($disc?->getParent()) $notice->addCodewey($spec);
-          }, $this->deweRep->findBy(['nom' => $value]));
+          // Gestion des spécialités
+          if (str_contains($key, 'lassification')) {
+            $disciplineGroupsByParent = [];
+            foreach ($class->taxonPath->taxons as $taxon) {
+              $spec = $taxon->entry[0]?->value;
+              $exist = $this->discRep->findOneBy(['nom' => $spec]);
+              if ($exist) {
+                $disc = $exist->getParent();
+                $champDisc = $disc?->getParent();
+                if ($champDisc && $disc) {
+                  $groupKey = $champDisc->getId() . '-' . $disc->getId();
+                  if (!isset($disciplineGroupsByParent[$groupKey])) {
+                    $this->em->persist($notice);
+                    $group = new \App\Entity\DisciplineGroup();
+                    $group->setChampDisc($champDisc);
+                    $group->setDiscipline($disc);
+                    $notice->addDisciplineGroup($group);
+                    $disciplineGroupsByParent[$groupKey] = $group;
+                    $this->em->persist($group);
+                  }
+                  $disciplineGroupsByParent[$groupKey]->addSpecialite($exist);
+                }
+              }
+            }
+          }
+
+          // Gestion des Deweys
+          if (str_contains($key, 'CDD 22')) {
+            $deweyGroupsByParent = [];
+            foreach ($class->taxonPath->taxons as $taxon) {
+              $spec = $taxon->entry[0]?->value;
+              $id = $taxon->id ?? null;
+
+              // code dewey original
+              $exist = $this->deweRep->findOneBy(['nom' => $spec]);
+              if ($exist) {
+                $disc = $exist->getParent();
+                $champDisc = $disc?->getParent();
+                if ($champDisc && $disc) {
+                  $groupKey = $champDisc->getId() . '-' . $disc->getId();
+                  if (!isset($deweyGroupsByParent[$groupKey])) {
+                    $group = new \App\Entity\DeweyGroup();
+                    $group->setDewey($champDisc);
+                    $group->setDivision($disc);
+                    $notice->addDeweyGroup($group);
+                    $deweyGroupsByParent[$groupKey] = $group;
+                    $this->em->persist($group);
+                  }
+                  $deweyGroupsByParent[$groupKey]->addCodewey($exist);
+                }
+              } else {
+                // code dewey personnaliser
+                $code = "http://dewey.info/class/" . $id . "/";
+                $existPerso = $this->dewePersoRep->findOneBy(['code' => $code]);
+                if ($existPerso) {
+                  $notice->addDeweyPerso($existPerso);
+                } else {
+                  $deweyPerso = new DeweyPerso();
+                  $deweyPerso->setCode($code);
+                  $deweyPerso->setNom($spec);
+                  $this->em->persist($deweyPerso);
+                  $this->em->flush();
+                  $notice->addDeweyPerso($deweyPerso);
+                }
+              }
+            }
+          }
         } elseif (str_contains($class->purpose?->value, 'educational')) $notice->setObjectif($class->description[0]?->string->value);
+
+
       }
       $this->em->persist($notice);
     }
 
     $io->progressFinish();
     $this->em->flush();
-    $output->writeln("Suplom imported successfully ! count of suplom with creator not found : ".count($notfound));
-    foreach ($notfound as $nf) {
-      $output->writeln("title : " . $nf['titre']);
-      $output->writeln("link : " . $nf['lien'] . "\n");
-    }
+    $output->writeln("Suplom imported successfully ! count of suplom with creator not found : ". count($notfound));
+    /*foreach ($notfound as $nf) {
+      $output->writeln("title : " . $nf['titre']. "\n");
+    }*/
     return Command::SUCCESS;
   }
+}
+// Ajout d'une fonction de conversion PT50H en jours/heures car cela causait des erreurs lors de la modification de certaines notices
+function normalizeDuration($duration) {
+  // Si la durée est du type PT50H, convertis en jours/heures
+  if (preg_match('/^PT(\d+)H$/', $duration, $matches)) {
+    $hours = (int)$matches[1];
+    $days = intdiv($hours, 24);
+    $restHours = $hours % 24;
+    $result = 'P';
+    if ($days > 0) $result .= $days . 'D';
+    $result .= 'T';
+    if ($restHours > 0) $result .= $restHours . 'H';
+    return $result;
+  }
+  return $duration;
 }
