47f458bcb9222d81b661943de73dd3964748d443
Louis Ferret <louis.ferret@coexya.eu>
2025-07-24 16:19:07 +0200

    Feat[UIN-181] : Correction lors de la creation des notices au format XML lors de l'indexation suite à l'ajout des group dewey et discipline Indexations dans des dossiers différents lorsque la notice doit être indexé dans l'entrepot JOAI

--- lunt-backoffice/src/Entity/Notice.php
+++ lunt-backoffice/src/Entity/Notice.php
@@ -916,6 +916,30 @@ class Notice
         if (!$codeDew) return '<span class="badge bg-secondary">Aucune</span>';
         return implode('<br>', array_map(fn($s) => '<span class="badge badge-custom">'.$s->getNom().'</span>', $codeDew));
     }
+    public function getAllDeweyGroup(): array
+    {
+      $codeweys = [];
+      // Ajoute les Dewey classiques via les groupes
+      foreach ($this->getDeweyGroups() as $group) {
+        foreach ($group->getCodeweys() as $code) {
+          if (!in_array($code, $codeweys, true)) {
+            $codeweys[] = $code;
+          }
+        }
+      }
+      return $codeweys;
+    }
+    public function getAllDeweyPerso(): array
+    {
+      $codeweys = [];
+      // Ajoute les DeweyPerso sélectionnés
+      foreach ($this->getDeweyPersos() as $perso) {
+        if (!in_array($perso, $codeweys, true)) {
+          $codeweys[] = $perso;
+        }
+      }
+      return $codeweys;
+    }
 
     #[Assert\Callback]
     public function validateDeweyGroups(ExecutionContextInterface $context): void
