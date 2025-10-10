285bac8e47450411120bf5a8a4b86bd00c7e2a24
DIBOS Ophélia <ophelia.dibos@coexya.eu>
2025-10-10 16:16:22 +0200

    Fix [UPDT-7] Doctrine DB: removing useless migrations

--- lunt-backoffice/migrations/Version20251010121916.php
+++ lunt-backoffice/migrations/Version20251010121916.php
@@ -20,7 +20,6 @@ final class Version20251010121916 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_20FD592C6C6E55B5 ON etablissement');
         $this->addSql('DROP INDEX UNIQ_4BDFF36B737992C9 ON niveau');
     }
 
@@ -28,6 +27,5 @@ final class Version20251010121916 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE UNIQUE INDEX UNIQ_4BDFF36B737992C9 ON niveau (ordre)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_20FD592C6C6E55B5 ON etablissement (nom)');
     }
 }
